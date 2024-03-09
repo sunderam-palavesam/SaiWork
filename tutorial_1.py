a7db69e4a1bdf0f86ae45c00be9aede51bdbdfef
sunderam-palavesam <53272631+sunderam-palavesam@users.noreply.github.com>
2024-03-09 08:48:21 +0000

    add tutotrial 1

---
+++ tutorial_1.py
@@ -0,0 +1,69 @@
+#print(f'My name is Sairaj')
+#now lets learn about variables
+# datatypes 
+
+"""
+Day 1 - topics covered 
+1 - print
+2 - variabels and datatypes
+3- type conversion
+4 - garbage collector
+5 - variable  references
+"""
+"""
+input1 = "2"
+input2 = "4"
+print(type(input1))
+print(type(input2))
+result =  input1 + input2
+
+print(f'My result is - {result}')
+
+
+
+mylist = [1,2,3,4,5]
+#print(type(myList))
+print(f'My List is - {mylist}')
+for myelement in mylist:
+    print(f'My element is - {myelement}')
+"""
+
+"""    
+mydict={'VM':50,'SM':40, 'SRM':18, 'PVM':12}
+print(f'My Dict is - {mydict}')
+for mykey,myvalue in mydict.items():
+    print(f' {mykey} is  {myvalue} years old')
+"""
+
+
+"""
+#list comprehensions
+squares = [n * n for n in range(1,10) if n%2 != 0]
+print(squares)
+
+"""
+
+#list1 = ["SM","PM","VM","SM","LP", "SM"]
+#list1 = [4,3,5,6,1]
+#list1.append("SR")
+#for l in list1:    
+#    print(l)
+"""
+print(f"-------------------")
+
+#tuple1 = ("SM","PM","VM","SM","LP","SM")
+tuple1 = (("SM", 18, "College"),("PM",13, "School"))
+for n,a,c in tuple1:
+    print(f"name is {n} and college is {c}")
+
+"""
+
+fb = {"A","B","C","D","E","F"}
+ck = {"P","Q","R","S","T","U"}
+
+#union , any sport
+fb.union(ck)
+fb.difference
+
+
+
